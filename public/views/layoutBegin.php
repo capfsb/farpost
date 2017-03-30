aa0415cf3fa1540aa88de65c4ea3b2df90cbb74e
Denis <you@example.com>
2017-03-31 03:19:01 +1000

    ie cache fix

--- public/views/layoutBegin.php
+++ public/views/layoutBegin.php
@@ -1,9 +1,9 @@
+<!DOCTYPE html>
 <?php
 /*error_reporting(E_ALL);
 ini_set('display_errors', 1);*/
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
--- public/views/layoutEnd.php
+++ public/views/layoutEnd.php
@@ -1,5 +1,6 @@
 </div>
 <script src="//cdn.jsdelivr.net/jquery/2.2.4/jquery.min.js"></script>
+<script>$.ajaxSetup({cache: false});</script>
 <script src="/js/modalFactory.js"></script>
 <script src="/js/notesModal.js"></script>
 <script src="/js/modalSamples.js"></script>
